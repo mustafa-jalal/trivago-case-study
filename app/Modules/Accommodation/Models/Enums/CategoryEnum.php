928e72380ef06fac844bf9ff751d5d64757e91c5
mustafa <mostafa.jalal@makeorder.com>
2023-09-27 09:30:00 +0300

    create categories enum

---
+++ app/Modules/Accommodation/Models/Enums/CategoryEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\Accommodation\Models\Enums;
+
+enum CategoryEnum
+{
+    case HOTEL;
+    case ALTERNATIVE;
+    case HOSTEL;
+    case LODGE;
+    case RESORT;
+    case GUEST_HOUSE;
+}
